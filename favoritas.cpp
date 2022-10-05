110b6311cf0c4b7e46cdc457100b9eea56dd2906
Shender04 <115058061+Shender04@users.noreply.github.com>
2022-10-04 22:49:24 -0600

    Update favoritas.cpp

--- favoritas.cpp
+++ favoritas.cpp
@@ -8,38 +8,41 @@ void Favoritas::insertarFav(Cancion *_cancion){
     }else{
         fin ->sig = _cancion;
         _cancion->ant = fin;
-
+        _cancion->sig = inicio;
+        inicio->ant=_cancion;
+        fin = _cancion;
+        
     }
-    fin = _cancion;
-    fin->sig = inicio;
+    
 }
 
 void Favoritas::eliminarFav(Cancion *_cancion){
     if (_cancion ->getNombre()==inicio->getNombre()){
-        Cancion *eliminado = inicio;
-        inicio = inicio->sig;
-        eliminado ->sig=NULL;
+        if(inicio->getNombre()==fin->getNombre()){
+            inicio=fin=NULL;
+        }
+        else{
+            fin->sig=inicio->sig;
+            inicio=inicio->sig;
+            inicio->ant=fin;  
+        }
     }
 
     else{
-        Cancion *cancion_anterior=inicio;
-        Cancion *tmp = inicio;
-        while (tmp!= NULL){
-            if (tmp->getNombre()== _cancion->getNombre()){
-                cancion_anterior->sig=tmp->sig;
-                tmp->sig = NULL;
-            }
-            cancion_anterior=tmp;
-            tmp=tmp->sig;
+        Cancion *cancion_eliminar = buscar(_cancion);
+        if (cancion_eliminar->ant!=NULL){
+            cancion_eliminar->ant->sig=cancion_eliminar->sig;
         }
-
+        if (cancion_eliminar->sig!=NULL){
+            cancion_eliminar->sig->ant=cancion_eliminar->ant;
+        }
     }
-
 }
 
 Cancion * Favoritas::buscarFav(Cancion *_cancion){
     Cancion*tmp=inicio;
-    while (tmp!=NULL){
+    tmp=tmp->sig;
+    while (tmp!=inicio){
         if(tmp->getNombre()==_cancion->getNombre()){
             return tmp;
         }
